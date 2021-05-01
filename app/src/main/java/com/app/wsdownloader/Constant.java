b30e87aeed162184f98bdd924676b34460fceb74
pfaman <amantheiit@gmail.com>
2021-05-01 18:47:47 +0530

    This App will help to downlaod whatsapp status

---
+++ app/src/main/java/com/app/wsdownloader/Constant.java
@@ -0,0 +1,8 @@
+package com.app.wsdownloader;
+
+public class Constant {
+
+    public static final String FOLDER_NAME="/whatsApp/";
+    public static final String SAVE_FOLDER_NAME="/WS Downloader/";
+
+}
